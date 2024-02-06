41d33d787c64f298079ea7c0fec142fcbf71e62f
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-02-05 17:07:22 -0700

    Menu Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,27 @@ import React from "react";
  
 const Navbar = () => {
     return (
-        <div>
-            <h1>Testing</h1>
-        </div>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Main Navigation Menu">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Expand at md</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/budget">Budget</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/paycheck">Paycheck</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
  
