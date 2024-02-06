612bed798f4b31fafd1767b71e7124433ae0d426
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-02-06 15:25:14 -0700

    Added Vars to calc

--- src/pages/Paycheck.jsx
+++ src/pages/Paycheck.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
  
 
-  function WithholdingState({ wages, payperiods, allowance, addtional }) {
+  function WithholdingState({ wages, payperiods, allowance, addtional, hours }) {
+    
+    var yearly = wages * payperiods;
+    var withholding = yearly - allowance;
+    var percentage = withholding * 0.044;
+    var excatAmount = percentage  / payperiods;
+    var coloradoFML = hours * 0.072;
+
     return (
       <ul>    
         <li>Paycheck Gross Wages: {wages}</li>
@@ -10,14 +17,13 @@ import Navbar from "../components/Navbar";
         <li>Withholding Allowance: {allowance}</li>
         <li>Addtional Allowance: {addtional}</li>
         <hr></hr>
-        <li>Estimated Yearly Income: ${wages * payperiods}</li>
-        yearly = {wages * payperiods}
-        <li>Withholding Yearly: {wages * payperiods - allowance}</li>
-        <li>Percentage: {wages * payperiods - allowance * 0.044}</li>
-        <li>Payperiod Check: Percentage / Number of Payperiods</li>
+        <li>Estimated Yearly Income: ${yearly}</li>
+        <li>Withholding Yearly: {withholding}</li>
+        <li>Percentage: {percentage}</li>
+        <li>Payperiod Check: {excatAmount}</li>
         <li>State Withholding (rounded up): round</li>
-        <li>Colorado FML: Hours worked X 0.0072</li>
-        <li>Total: Withholding + FML</li>
+        <li>Colorado FML: {coloradoFML}</li>
+        <li>Total: {excatAmount + coloradoFML}</li>
 
       </ul>
     );
@@ -29,9 +35,10 @@ const Paycheck = () => {
             <Navbar></Navbar>
             <h1>Testing</h1>
 
-            <WithholdingState wages={480} payperiods={26} allowance={5000} addtional={0}/>
+            <WithholdingState wages={480} payperiods={26} allowance={5000} addtional={0} hours={30}/>
         </div>
     );
 };
  
 export default Paycheck;
+
