5fa4e8fc5842c8bd2ba21aafb94584e7bfec0f92
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-02-29 15:42:48 -0700

    Added list of tables

--- model/schema.js
+++ model/schema.js
@@ -1,8 +1,56 @@
+//https://watermelondb.dev/docs/Schema
 import { appSchema, tableSchema } from '@nozbe/watermelondb'
 
-export default appSchema({
+export const mySchema = appSchema({
   version: 1,
   tables: [
-    // We'll add tableSchemas here later
+    tableSchema({
+        name: 'users',
+        columns: [
+            //I Dont think an ID is needed it might be implied?
+          { name: 'username', type: 'string' },
+          { name: 'email', type: 'string'},
+          { name: 'password', type: 'string' },
+        ]
+      }),
+      tableSchema({
+        name: 'budgets',
+        columns: [
+            //I Dont think an ID is needed it might be implied?
+          { name: 'username', type: 'string' },
+          { name: 'email', type: 'string'},
+          { name: 'password', type: 'string' },
+        ]
+      }),
+
+      tableSchema({
+        name: 'catagories',
+        columns: [
+            //I Dont think an ID is needed it might be implied?
+          { name: 'username', type: 'string' },
+          { name: 'email', type: 'string'},
+          { name: 'password', type: 'string' },
+        ]
+      }),
+
+      tableSchema({
+        name: 'goals',
+        columns: [
+            //I Dont think an ID is needed it might be implied?
+          { name: 'username', type: 'string' },
+          { name: 'email', type: 'string'},
+          { name: 'password', type: 'string' },
+        ]
+      }),
+
+      tableSchema({
+        name: 'taxes',
+        columns: [
+            //I Dont think an ID is needed it might be implied?
+          { name: 'username', type: 'string' },
+          { name: 'email', type: 'string'},
+          { name: 'password', type: 'string' },
+        ]
+      }),
   ]
 })
