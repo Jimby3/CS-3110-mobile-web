19344c292e3af48191b1ea1c91b7f64740c783dd
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-03-17 15:06:10 -0600

    Attempt at model relationships

--- model/budget.js
+++ model/budget.js
@@ -2,4 +2,7 @@ import { Model } from '@nozbe/watermelondb'
 
 export default class Budget extends Model {
     static table = 'budgets'
+    static associations = {
+      goals: { type: 'has_many', foreignKey: 'budget_id' },
+    }
   }
--- model/goal.js
+++ model/goal.js
@@ -2,4 +2,7 @@ import { Model } from '@nozbe/watermelondb'
 
 export default class Goal extends Model {
     static table = 'goals'
+    static associations = {
+        budgets: { type: 'belongs_to', key: 'budget_id' },
+      }
   }
