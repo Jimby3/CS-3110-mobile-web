cc056a9d6347392f82e0c2f07bcf474eb7711ab3
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-04-09 15:47:47 -0600

    Tried setting up user to catagories

--- model/category.js
+++ model/category.js
@@ -2,4 +2,7 @@ import { Model } from '@nozbe/watermelondb'
 
 export default class Category extends Model {
     static table = 'categories'
+    static associations = {
+      users: { type: 'belongs_to', key: 'user_id' },
+    }
   }
--- model/user.js
+++ model/user.js
@@ -2,4 +2,7 @@ import { Model } from '@nozbe/watermelondb'
 
 export default class User extends Model {
     static table = 'users'
+    static associations = {
+      categorys: { type: 'has_many', foreignKey: 'catagory_id' },
+    }
   }
