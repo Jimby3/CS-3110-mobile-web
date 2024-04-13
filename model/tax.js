f69582162aa151ebfd393bc96eac73236150a407
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-04-13 17:35:13 -0600

    Fixed schema and all models

--- model/tax.js
+++ model/tax.js
@@ -1,11 +1,17 @@
 import { Model } from '@nozbe/watermelondb'
+import { field, relation } from "@nozbe/watermelondb/decorators";
 
 export default class Tax extends Model {
     static table = 'taxes'
 
+    static associations = {
+      users: { type: 'belongs_to', key: 'users_id' },
+    }
 
-    @text('state_withholding') state_withholding
-    @number('state_id') state_id
-    @text('federal_withholding') federal_withholding
-    @number('federal_id') federal_id
+    @relation('users', 'user_id') user
+
+    @field('state_withholding') state_withholding
+    @field('state_id') state_id
+    @field('federal_withholding') federal_withholding
+    @field('federal_id') federal_id
   }
--- model/user.js
+++ model/user.js
@@ -1,12 +1,17 @@
 import { Model } from '@nozbe/watermelondb'
+import { field, children } from "@nozbe/watermelondb/decorators";
 
 export default class User extends Model {
     static table = 'users'
     static associations = {
-      categorys: { type: 'has_many', foreignKey: 'catagory_id' },
+      categories: { type: 'has_many', foreignKey: 'catagory_id' },
+      taxes: { type: 'has_many', foreignKey: 'taxes_id' },
     }
 
-    @text('username') username
-    @text('email') email
-    @text('password') password
+    @field('username') username
+    @field('email') email
+    @field('password') password
+
+    @children('budgets') budgets
+    @children('taxes') taxes
   }
