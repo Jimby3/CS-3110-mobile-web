2c84fa199888d4507d55853af805fcb876845f5f
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-04-13 17:39:58 -0600

    Starting mirgrations process

--- src/index.js
+++ src/index.js
@@ -11,22 +11,12 @@ import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
 
 import schema from './model/schema'
 import migrations from './model/migrations'
-import User from './model/User'
+import { mySchema } from '../model/schema';
 
 // First, create the adapter to the underlying database:
 const adapter = new SQLiteAdapter({
-  schema,
-  // (You might want to comment it out for development purposes -- see Migrations documentation)
+  schema: mySchema,
   migrations,
-  // (optional database name or file system path)
-  // dbName: 'myapp',
-  // (recommended option, should work flawlessly out of the box on iOS. On Android,
-  // additional installation steps have to be taken - disable if you run into issues...)
-  jsi: true, /* Platform.OS === 'ios' */
-  // (optional, but you should implement this method)
-  onSetUpError: error => {
-    // Database failed to load -- offer the user to reload the app or log out
-  }
 })
 
 // Then, make a Watermelon database from it!
