562cd5d4ce61c04fb1d52ccaa5ed9a0308e61770
jimby3 <jimever333@gmail.com>
2024-04-18 09:39:40 -0600

    fixed mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
  
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Main Navigation Menu">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Expand at md</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Main Navigation Menu">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Expand at md</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbar">
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/budget">Budget</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/budget">Budget</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/paycheck">Paycheck</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/paycheck">Paycheck</a>
                     </li>
                 </ul>
                 </div>
