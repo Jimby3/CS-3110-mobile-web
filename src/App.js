dbfd6149c85d34dde03966c9b6191fb3f35229e0
jimby3 <jimever333@gmail.com>
2024-04-18 09:50:38 -0600

    rid of logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
-//import Navbar from "./components/Navbar";
 
 import {
   BrowserRouter as Router,
@@ -28,18 +26,7 @@ function App() {
         </Routes>
       </Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          basic starting webpage for CS 3110
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import {Link} from "react-router-dom";
  
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h1>Testing</h1>
+            <h1>Homepage</h1>
+            <p>need to figure out what to put here</p>
+            <div>
+                <Link to="/paycheck">
+                    <button>Paycheck Calc</button>
+                </Link>
+                <Link to="/budget">
+                    <button>Budget</button>
+                </Link>
+            </div>
         </div>
+
     );
 };
  
