66d7002a0dae76fd3354f5302ad365eee811a033
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-04-18 19:57:03 -0600

    Basic firestore pulling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { collection, getDocs } from 'firebase/firestore'
 //import Navbar from "./components/Navbar";
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  useActionData,
 } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Paycheck from "./pages/Paycheck";
 import Budget from "./pages/Budget";
 import ErrorPage from "./pages/ErrorPage";
+import { db } from "./firebase-config";
 
 
 function App() {
+  const [users, setUsers] = useState([])
+  const usersCollectionRef = collection(db, "users")
+  useEffect(() => {
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      //Array of users basically
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+    };
+
+    getUsers()
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App"> {users.map((user) => { return <div><h1>Name: {user.email}</h1></div>})}
       <Router>
         <Routes>
             <Route index element={<Home />}/>
