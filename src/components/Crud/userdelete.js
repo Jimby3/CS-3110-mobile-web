9a7b826767a961325bce8d784d83bcbf11f517e2
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-04-21 17:40:42 -0600

    Created CRUD functions for users (blank)

---
+++ src/components/Crud/userdelete.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { collection, getDocs, addDoc } from 'firebase/firestore'
+import { db } from "./firebase-config";
+
+const UserDelete = () => {
+    
+}
+
+export default UserDelete;
