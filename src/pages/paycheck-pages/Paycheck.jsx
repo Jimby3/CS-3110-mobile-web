bedb9da8661ff07badcc80ece85f9a45c4a6b225
ThatOneWanderingWeirdo <keyleasia@gmail.com>
2024-04-27 20:34:12 -0600

    Temp hour saver

--- src/pages/paycheck-pages/Paycheck.jsx
+++ src/pages/paycheck-pages/Paycheck.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
 const Paycheck = () => {
+  const [hours, setHours] = useState('');
+
+  const hoursChange = (event) => {
+    setHours(event.target.value); 
+  };
+
     return (
         <div>
             <Navbar></Navbar>
 
             <form>
               <label for="hoursInput">Hours Worked</label><br></br>
-              <input type="number" id="hoursInput" name="hoursInput"></input><br></br>
+              <input type="number" id="hoursInput" name="hoursInput" value={hours} onChange={hoursChange}></input><br></br>
+              <button>Save hours</button>
               <p>This is where the gross pay will go</p>
               <Link to="../paycheck-config">
                 <button>Configure Withholding Amount/Config</button>
