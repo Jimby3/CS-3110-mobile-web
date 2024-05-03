dca2b6a3d92408d281b8777a343216d84ff4687c
jimby3 <jimever333@gmail.com>
2024-05-02 20:46:44 -0600

    just fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../css/styles.css';
 
@@ -6,25 +7,24 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark navbar-custom" aria-label="Main Navigation Menu">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">CS3110</a>
+                <Link className="navbar-brand" to="/">CS3110</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbar">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/CS-3110-mobile-web/">Home</a>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/CS-3110-mobile-web/paycheck-budget">Budget</a>
+                            <Link className="nav-link" to="/paycheck-budget">Budget</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/CS-3110-mobile-web/paycheck">Paycheck</a>
+                            <Link className="nav-link" to="/paycheck">Paycheck</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/CS-3110-mobile-web/savings">Savings Goals</a>
+                            <Link className="nav-link" to="/savings">Savings Goals</Link>
                         </li>
-
                     </ul>
                 </div>
             </div>
@@ -33,4 +33,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
